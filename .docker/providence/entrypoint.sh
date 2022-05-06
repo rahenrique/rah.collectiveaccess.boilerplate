def5949cb0659703ff23b022168d9fc19f65dc53
Rafael Henrique <rafael@rah.com.br>
2022-05-06 17:21:53 -0300

    feat: :tada: Creates a Collective Access boilerplate with Docker
    Docker containers for CA (Providence app), MySQL database;

---
+++ .docker/providence/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# cd /var/www/html/providence/
+
+# git checkout
+# if ! git ls-files >& /dev/null; then
+#     git clone --depth 1 --branch 1.7.14 https://github.com/collectiveaccess/providence.git . 2>&1
+#     # cp setup.php-dist setup.php
+#     chmod -R 777 /var/www/html/providence/app/tmp
+#     chmod -R 777 /var/www/html/providence/app/log
+#     chmod -R 777 /var/www/html/providence/vendor/ezyang/htmlpurifier/library/HTMLPurifier/DefinitionCache
+#     chmod -R 777 /var/www/html/providence/media
+# fi;
+
+# composer install -o -d /var/www/html
